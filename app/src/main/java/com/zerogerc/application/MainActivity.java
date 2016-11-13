1e34d7566d5bf4b913b39689c55f609c674fc052
Uladzislau Sazanovich <zerogerc@gmail.com>
2016-11-13 13:59:44 +0300

    Toogle button

--- app/src/main/java/com/zerogerc/application/MainActivity.java
+++ app/src/main/java/com/zerogerc/application/MainActivity.java
@@ -2,12 +2,20 @@ package com.zerogerc.application;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ToggleButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_toggle)
+    ToggleButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
