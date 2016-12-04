95aae627c390211288ea6788cfa82f56c947a872
Uladzislau Sazanovich <zerogerc@gmail.com>
2016-12-04 20:00:10 +0300

    TimePicker experiments

--- app/src/main/java/com/zerogerc/application/MainActivity.java
+++ app/src/main/java/com/zerogerc/application/MainActivity.java
@@ -2,12 +2,22 @@ package com.zerogerc.application;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TimePicker;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.time_picker)
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        timePicker.setIs24HourView(true);
     }
 }
