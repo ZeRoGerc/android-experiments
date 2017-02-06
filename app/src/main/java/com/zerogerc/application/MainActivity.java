6051bec3ced8e678ec8a6a39c96dd7bd5671bd36
Uladzislau Sazanovich <zerogerc@gmail.com>
2017-02-06 16:20:40 +0300

    PopupWindow for android 4 and higher

--- app/src/main/java/com/zerogerc/application/MainActivity.java
+++ app/src/main/java/com/zerogerc/application/MainActivity.java
@@ -1,7 +1,17 @@
 package com.zerogerc.application;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.ChangeBounds;
+import android.transition.Fade;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setOnClickListener((view) -> {
+            LayoutInflater inflater = LayoutInflater.from(this);
+            View layout = inflater.inflate(R.layout.layout_popup, null);
+            PopupWindow popupWindow = new PopupWindow(layout, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.popup_background_mtr));
+            popupWindow.showAtLocation(view, Gravity.END, 0, 0);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Fade fade = new Fade(Fade.IN);
+                fade.setDuration(200);
+                popupWindow.setEnterTransition(new ChangeBounds(this, null));
+            }
+
+            popupWindow.showAsDropDown(view);
+        });
+
     }
 }
